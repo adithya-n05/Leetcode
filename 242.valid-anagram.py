6523f31bdfecedc74e7f8b31a22b38ddcfc0663c
Adithya <adithya.narayanan01@gmail.com>
2023-07-26 04:29:24 +0800

    More questions

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -12,6 +12,29 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        dictOfLetters = {}
+        dictOfLettersT = {}
+
+        for i in range(len(s)):
+            if s[i] in dictOfLetters:
+                dictOfLetters[s[i]] = dictOfLetters[s[i]] + 1
+            else:
+                dictOfLetters[s[i]] = 1
         
+        for i in range(len(t)):
+            if t[i] not in dictOfLetters:
+                return False
+        
+        for i in range(len(t)):
+            if t[i] in dictOfLettersT:
+                dictOfLettersT[t[i]] = dictOfLettersT[t[i]] + 1
+            else:
+                dictOfLettersT[t[i]] = 1
+
+        if dictOfLettersT != dictOfLetters:
+            return False
+        
+        return True
+
 # @lc code=end
 
